23a5176eb1537febb7c52aeebd7867d4b4ae5a29
Fed Reggiardo <bksgtr@gmail.com>
2016-10-26 10:04:24 -0400

    Completed exercise

---
+++ Model.elm
@@ -0,0 +1,26 @@
+module Model exposing (..)
+
+
+type alias Model =
+    { currentText : String
+    , direction : Direction
+    , selectedKey : String
+    }
+
+
+type Direction
+    = TextToEmoji
+    | EmojiToText
+
+
+init : Model
+init =
+    { currentText = ""
+    , direction = TextToEmoji
+    , selectedKey = defaultKey
+    }
+
+
+defaultKey : String
+defaultKey =
+    "ð"
---
+++ Update.elm
@@ -0,0 +1,27 @@
+module Update exposing (..)
+
+import Model
+
+
+type Msg
+    = SetCurrentText String
+    | ToggleDirection
+    | SetSelectedKey String
+
+
+update : Msg -> Model.Model -> Model.Model
+update msg model =
+    case msg of
+        SetCurrentText newText ->
+            { model | currentText = newText }
+
+        ToggleDirection ->
+            case model.direction of
+                Model.TextToEmoji ->
+                    { model | direction = Model.EmojiToText }
+
+                Model.EmojiToText ->
+                    { model | direction = Model.TextToEmoji }
+
+        SetSelectedKey newKey ->
+            { model | selectedKey = newKey }
